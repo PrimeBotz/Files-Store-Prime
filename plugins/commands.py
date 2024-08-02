cac195f650428f6768381caf29bab4ad991effd9
Prime5522 <sahidhasannayem@gmail.com>
2024-08-02 17:01:50 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,10 +12,8 @@ from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
-from pyrogram.errors import *
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, 
 from utils import verify_user, check_token, check_verification, get_token
 from config import AUTH_CHANNEL, 
 import re
