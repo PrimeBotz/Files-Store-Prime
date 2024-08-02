800f9a97b72fb1a8582b66c134e839a096730e31
Prime5522 <sahidhasannayem@gmail.com>
2024-08-02 14:36:07 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,9 +13,9 @@ from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
 from pyrogram.errors import *
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, 
 from utils import verify_user, check_token, check_verification, get_token
-from config import AUTH_CHANNEL, *
+from config import AUTH_CHANNEL, 
 import re
 import json
 import base64
