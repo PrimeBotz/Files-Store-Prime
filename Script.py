e1e7dd570f03848096a614d6b7a9fec0f6aa6493
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-12-10 07:35:16 +0600

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,11 +13,11 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @Prime_Movies4U {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
 sɪᴢᴇ ⚙️: {file_size}
 
-Jᴏɪɴ Now ➠ [𝐌𝐎𝐕𝐈𝐄𝐒 𝐑𝐄𝐐𝐔𝐄𝐒𝐓 𝐆𝐑𝐎𝐔𝐏 ✨](https://t.me/Prime_Movies4U/143)</b>""" 
+⚡ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [ᴘʀɪᴍᴇ ʙᴏᴛᴢ](https://t.me/Prime_Botz)</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
